aab98b346a15591f1aa382b8f301e88060d97183
Joël Elunda <joel.elunda.01@gmail.com>
2020-08-30 09:56:37 +0200

    Google Translation with Flutter

---
+++ android/app/src/main/kotlin/com/example/congo_translator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.congo_translator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'screens/HomePage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Congo Translator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.blue[600],
+      ),
+      home: HomePage(title: 'Congo Translator'),
+    );
+  }
+}
